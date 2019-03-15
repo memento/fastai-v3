df411a2611ba593ea16fd69f11fc34c77a9bcdf2
Alexandre <dev@kxz3.tk>
2019-03-15 23:39:50 +0100

    Now the launchFresh.sh do the following : Stops container, Removes all containers (like neural_marvel_instance_1) based on the image neural_marvel, Removes the image neural_marvel, Re-builds the container and finally Launches container (detach process)

--- launchFresh.sh
+++ launchFresh.sh
@@ -1,8 +1,15 @@
+#Author : Alexandre DIEUL
+#
+#Note : Remember a you build an image (named neural_marvel) and from that image, 
+# you can run multiple containers (here, just one named neural_marvel_instance_1)
+#
 #Stop container
-sudo docker container stop neural_marvel
-#Remove container
-sudo docker container rm neural_marvel
+sudo docker container stop neural_marvel_instance_1
+#Remove all containers (like neural_marvel_instance_1) based on the image neural_marvel
+sudo docker ps -a | awk '{ print $1,$2 }' | grep neural_marvel | awk '{print $1 }' | sudo xargs -I {} docker rm {}
+#Remove the image neural_marvel
+sudo docker rmi neural_marvel
 #Re-build container
 sudo docker build . -t neural_marvel
 #Launch container (detach process)
-sudo docker run -p 5042:5042 -d neural_marvel
+sudo docker run -p 5042:5042 --name neural_marvel_instance_1 -d neural_marvel
