30e7359ee99349bdf3512d46cd79d9d624375d39
Alexandre <dev@kxz3.tk>
2019-03-16 00:42:34 +0100

    False good idea. This script is useless

--- launchFresh.sh
+++ launchFresh.sh
@@ -1,7 +1,7 @@
-#Author : Alexandre DIEUL
+# Author : Alexandre DIEUL
 #
-#Note : Remember a you build an image (named neural_marvel) and from that image, 
-# you can run multiple containers (here, just one named neural_marvel_instance_1)
+# Note : Remember that we build an image (named neural_marvel) and from that image, 
+#	we can run multiple containers (here, just one named neural_marvel_instance_1)
 #
 #Stop container
 sudo docker container stop neural_marvel_instance_1
@@ -9,7 +9,7 @@ sudo docker container stop neural_marvel_instance_1
 sudo docker ps -a | awk '{ print $1,$2 }' | grep neural_marvel | awk '{print $1 }' | sudo xargs -I {} docker rm {}
 #Remove the image neural_marvel
 sudo docker rmi neural_marvel
-#Re-build container
+#Re-build the container
 sudo docker build . -t neural_marvel
 #Launch container (detach process)
 sudo docker run -p 5042:5042 --name neural_marvel_instance_1 -d neural_marvel
